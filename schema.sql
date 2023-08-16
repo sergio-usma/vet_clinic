1041400a58b938d407aafb9b07aeb71b12c4b36d
Sergio Andres Usma <126805818+sergio-usma@users.noreply.github.com>
2023-08-16 17:09:41 -0500

    Add new columns and FK

--- schema.sql
+++ schema.sql
@@ -16,3 +16,35 @@ CREATE TABLE animals (
 
 ALTER TABLE animals
 ADD COLUMN species VARCHAR(50);
+
+-- THIRD PART --
+
+CREATE TABLE owners (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    full_name TEXT NOT NULL,
+    age INT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE species (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE animals
+DROP COLUMN species;
+
+ALTER TABLE animals
+ADD COLUMN species_id INT;
+
+ALTER TABLE animals
+ADD CONSTRAINT fk_species
+FOREIGN KEY (species_id) REFERENCES species(id);
+
+ALTER TABLE animals
+ADD COLUMN owner_id INT;
+
+ALTER TABLE animals
+ADD CONSTRAINT fk_owner
+FOREIGN KEY (owner_id) REFERENCES owners(id);
