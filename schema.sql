2214bf68bfe78219437c3bf3295709a5d3ec3443
Sergio Andres Usma <126805818+sergio-usma@users.noreply.github.com>
2023-08-17 15:39:16 -0500

    Add tables vets, specializations and visits

--- schema.sql
+++ schema.sql
@@ -48,3 +48,27 @@ ADD COLUMN owner_id INT;
 ALTER TABLE animals
 ADD CONSTRAINT fk_owner
 FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+-- FOURTH PART --
+CREATE TABLE vets (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name TEXT NOT NULL,
+    age INT,
+    date_of_graduation DATE,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE specializations (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    vet_id INT REFERENCES vets(id),
+    species_id INT REFERENCES species(id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE visits (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    animal_id INT REFERENCES animals(id),
+    vet_id INT REFERENCES vets(id),
+    date_of_visit DATE,
+    PRIMARY KEY (id)
+);
